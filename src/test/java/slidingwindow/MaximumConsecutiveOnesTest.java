24648e7277909cfad3b77a403e07e62f76f46fbb
Stephen Adehor <stevade.stephen@gmail.com>
2022-03-17 16:12:28 +0100

    add Maximum Consecutive Ones with k flips allowed test

--- src/test/java/slidingwindow/MaximumConsecutiveOnesTest.java
+++ src/test/java/slidingwindow/MaximumConsecutiveOnesTest.java
@@ -1,12 +1,18 @@
 package slidingwindow;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaximumConsecutiveOnesTest {
-    MaximumConsecutiveOnes underTest = new MaximumConsecutiveOnes();
+    MaximumConsecutiveOnes underTest;
+
+    @BeforeEach
+    public void setUp(){
+        underTest = new MaximumConsecutiveOnes();
+    }
 
     @Test
     void canCountConsecutiveOnesSuccessfully() {
@@ -17,4 +23,17 @@ class MaximumConsecutiveOnesTest {
         int result3 = underTest.countConsecutiveOnes(new int[]{1, 0, 1, 1, 0, 0, 1, 0, 1});
         assertThat(result3).isEqualTo(4);
     }
+
+    @Test
+    void canCountConsecutiveOnesSuccessfullyWithKFlipsAllowed(){
+        int result = underTest.countConsecutiveOnesWithKFlips(new int[]{1, 1, 0, 0, 1, 1, 1, 1, 1}, 2);
+        assertThat(result).isEqualTo(9);
+        int result2 = underTest.countConsecutiveOnesWithKFlips(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1}, 2);
+        assertThat(result2).isEqualTo(5);
+        int result3 = underTest.countConsecutiveOnesWithKFlips(new int[]{1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1}, 2);
+        assertThat(result3).isEqualTo(6);
+        int result4 = underTest.countConsecutiveOnesWithKFlips(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1}, 3);
+        assertThat(result4).isEqualTo(9);
+    }
+
 }
