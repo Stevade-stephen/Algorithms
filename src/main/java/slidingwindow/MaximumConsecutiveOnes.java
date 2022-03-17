b3eba9e39db59396dfac7505bc68ed28755ed437
Stephen Adehor <stevade.stephen@gmail.com>
2022-03-17 16:12:18 +0100

    add Maximum Consecutive Ones with k flips allowed

--- src/main/java/slidingwindow/MaximumConsecutiveOnes.java
+++ src/main/java/slidingwindow/MaximumConsecutiveOnes.java
@@ -3,7 +3,7 @@ package slidingwindow;
 
 /**
  * Maximum consecutive ones II. Given an array which only consists of 0s and 1s.
- * Write a code to find the maximum number of consecutive 1s in an array, if you can flip at most one 0.
+ * Write a code to find the maximum number of consecutive 1s in an array, if you can flip at most one zero.
  * For example:
  * Input: {1, 1, 0, 0, 1, 1, 1, 1, 1}
  * Output: 6
@@ -11,6 +11,7 @@ package slidingwindow;
  */
 public class MaximumConsecutiveOnes {
 
+    @SuppressWarnings(value = "all")
     public int countConsecutiveOnes(int [] arr){
         int maxConsecutiveOne = 0;
         int start = 0;
@@ -25,6 +26,30 @@ public class MaximumConsecutiveOnes {
             while (numberOfZerosEncountered > numberOfZerosToFlip){
                 if(arr[start] == 0){
                     numberOfZerosEncountered--;
+                }
+                start++;
+            }
+
+            maxConsecutiveOne = Math.max(maxConsecutiveOne, end - start + 1);
+        }
+        return maxConsecutiveOne;
+    }
+
+    //This is for if you can flip k zeros.
+    @SuppressWarnings(value = "all")
+    public int countConsecutiveOnesWithKFlips(int [] arr, final int k){ //[1,1,0,0,1,1,1,1,1]
+        int start = 0;
+        int maxConsecutiveOne = 0;
+        int numberOfZerosSeen = 0;
+
+        for(int end = 0; end < arr.length; end++){
+            if(arr[end] == 0){
+                numberOfZerosSeen++;
+            }
+
+            while (numberOfZerosSeen > k){
+                if(arr[start] == 0){
+                    numberOfZerosSeen--;
                 }
                 start++;
             }
