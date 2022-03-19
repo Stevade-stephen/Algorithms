d7165f9c733e35692e71b195fe30438b8d8ff42e
Stephen Adehor <stevade.stephen@gmail.com>
2022-03-19 20:38:31 +0100

    add more tests

--- src/test/java/slidingwindow/MaximumConsecutiveOnesTest.java
+++ src/test/java/slidingwindow/MaximumConsecutiveOnesTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaximumConsecutiveOnesTest {
-    MaximumConsecutiveOnes underTest;
+    private MaximumConsecutiveOnes underTest;
 
     @BeforeEach
     public void setUp(){
